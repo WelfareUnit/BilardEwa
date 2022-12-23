c59b3b685716ebd2c490520b3111d41a38e0cdd1
WelfareUnit <palczewskijan13@gmail.com>
2022-12-23 19:41:57 +0100

    Dodaj pliki projektów.

---
+++ main.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include "winbgi2.h"
+int oknoX = 1280, oknoY = 700;
+int stolX = oknoX - 40, stolY = oknoY - 40;
+void rysowaniestolu()
+{
+    //krawędzie stołu
+    line(40, 40, stolX, 40);
+    line(40, stolY, stolX, stolY);
+    line(40, 40, 40, stolY);
+    line(stolX, 40, stolX, stolY);
+    
+    //dziury w stole
+    setcolor(5);
+    circle(80, 80, 40);
+    circle(80, stolY - 40, 40);
+    circle(stolX - 40, 80, 40);
+    circle(stolX - 40, stolY - 40,40);
+
+}
+
+int main()
+{
+
+    graphics(oknoX, oknoY);
+    rysowaniestolu();
+    double xp[10], yp[10];        // wspolrzedne pilek
+    double xVp[10], yVp[10];      // skladowe predkosci pilek
+    int rp = 35; // promień piłki
+    wait();
+
+    return 0;
+}
